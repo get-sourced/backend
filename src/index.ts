9f86f20e47dfbc12efeea6e7fc8197887d72e583
sappiah085 <sappiah5689@gmail.com>
2025-02-20 14:25:00 +0100

    starter folder structure for backend

---
+++ src/index.ts
@@ -0,0 +1,52 @@
+import express, { Application } from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import mongoose from "mongoose";
+//For env File
+dotenv.config({ path: "./.env" });
+//main express app
+const app: Application = express();
+
+app.use(express.json({ limit: "50mb" }));
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+
+//on unhandled uncaughtExceptions
+process.on("uncaughtException", (err) => {
+  console.log("Uncaught exception", err);
+  process.exit(1);
+});
+if (process.env.DB_PASSWORD && process.env.DB_URL)
+  mongoose
+    .connect(process.env.DB_URL.replace("<password>", process.env.DB_PASSWORD))
+    .then(() => console.log("Connected to database"));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+const port = process.env.PORT ?? 8000;
+const server = app.listen(port, () => {
+  console.log(`Server is Fire at https://localhost:${port}`);
+});
+//error handler
+app.use(error);
+//on unhandledRedjections
+process.on("unhandledRejection", (err) => {
+  console.log("Error 🔥🔥", err);
+  server.close(() => {
+    process.exit(1);
+  });
+});
+
+process.on("SIGTERM", () => {
+  console.log("SIGTERM");
+  server.close(() => {
+    process.exit(1);
+  });
+});
+export default app;
